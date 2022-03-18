ac415bbbb99645400091e242a092c1dd8babc6b5
HUAWEU <101175020+HUAWEU@users.noreply.github.com>
2022-03-18 16:22:28 +0800

    edit variables

---
+++ Practical5/variables.py
@@ -0,0 +1,22 @@
+# On 7 March, 2022, the number of total cases of COVID-19 in the UK was stored in a.
+a = 19245301
+# On 7 March, 2021, the number of total cases of COVID-19 in the UK was sorted in b.
+b = 4218520
+# On 7 March, 2020, the number of total cases of COVID-19 in thr UK was sorted in c.
+c = 271
+# The difference between the numbers of cases in 2020 and 2021 was sorted in d.
+d = b-c
+print(d)
+# The difference between the numbers of cases in 2021 and 2022 was sorted in e.
+e = a-b
+print(e)
+# The comparison between d and e.
+# E is greater.The rate of new cases in 2021 was greater than 2020.
+print(e>d)
+
+#Booleans
+X = True
+Y = False
+W = X and Y
+print(W)
+

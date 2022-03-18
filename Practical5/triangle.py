49b314b423611454d62a2f24ec5e6748d835eae5
HUAWEU <101175020+HUAWEU@users.noreply.github.com>
2022-03-18 16:23:55 +0800

    triangle

---
+++ Practical5/triangle.py
@@ -0,0 +1,14 @@
+# Calculate the triangle sequence
+# the initial value of the total dots number called n
+# a for-loop of order of numbers
+#        calculate the value of the triangle sequence
+#        print the result
+
+# the initial value of the total dots number called n
+n = 0
+#a for-loop of order of munbers
+for i in range(1,11):
+#   calculate the value of the triangle sequence
+    n = i+n
+#   print the result
+    print("value",i," ",n)

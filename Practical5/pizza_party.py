e28b8b1c15ad4d5f3cf42ecd2cf09cef60a56cec
HUAWEU <101175020+HUAWEU@users.noreply.github.com>
2022-03-19 18:46:58 +0800

    edit pizza_party

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,12 +1,12 @@
 # cutting pizza
-#if the total number of pieces of pizza less than 64? 
+#if the total number of pieces of pizza less than 64?
 #     yes:keep cutting
 #     no: done!
 # the number of pieces of pizza is storted in p.
 # the given number of straight cuts is storted in n
 n = 0
 p =1
-#if the total number of pieces of pizza less than 64? 
+#if the total number of pieces of pizza less than 64?
 while p < 64:
    n = n+1
    p = (n**2 + n + 2) / 2
@@ -18,4 +18,3 @@ while p < 64:
 
 # done!
 print(str(n), "straight cuts can make", str(p), "pieces of pizza, there is enough pizza for each member of the class.")
-
